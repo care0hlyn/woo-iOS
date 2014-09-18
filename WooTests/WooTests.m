e3168ec0dea53f7fcd66d3f62d4847182efe2df2
Carolyn Huynh <ckhuynh20@gmail.com>
2014-09-17 23:15:15 -0700

    Initial Commit

---
+++ WooTests/WooTests.m
@@ -0,0 +1,34 @@
+//
+//  WooTests.m
+//  WooTests
+//
+//  Created by Carolyn Huynh on 9/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WooTests : XCTestCase
+
+@end
+
+@implementation WooTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
